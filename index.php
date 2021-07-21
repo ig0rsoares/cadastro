fc9b36194e5d4b0f73a33c1d2c6792b255e467cd
ig0rsoares <igor_soares15@live.com>
2021-07-20 22:39:59 -0300

    upando arquivos iniciais do modulo

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>TECTREINAMENTOS</title>
+
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+		<script>
+			// código javascript						
+		</script>
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <img src="imagens/logo.png" />
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="inscrevase.php">Inscrever-se</a></li>
+	            <li class="">
+	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+					<ul class="dropdown-menu" aria-labelledby="entrar">
+						<div class="col-md-12">
+				    		<p>Você possui uma conta?</h3>
+				    		<br />
+							
+								<br /><br />
+								
+							</form>
+						</form>
+				  	</ul>
+	            </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+	    <br>
+
+
+	    <div class="container">
+	    	<h1><strong>TECTREINAMENTOS INFORMÁTICA</strong></h1>
+	    	<hr>
+
+						<div class="container">
+							<div class="row">
+							    <div class="col-sm-3">
+							     <img src="imagens/logo_tec.png" alt="..." class="rounded-circle" max width="70%">
+							    </div>
+							    <div class="col-sm-9">
+							     
+							    <h2>Prof. Wagner Cardoso</h2>	
+							     <p>
+								Sou Profissional na área de Tecnologia e também apaixonado por jogos e cinema. <br>
+
+								Formado em Análise e Desenvolvimento de Sistemas e Pós - Graduado em Docência para Ensino Superior.<br>
+
+								Desde 2012 atuo em uma ONG na cidade de São Paulo - Brasil, onde realizo a manutenção de equipamentos e ministro cursos na área de informática.<br>
+
+								Me dedico ao empreendedorismo digital acreditando na democratização do conhecimento por intermédio da internet.<br>
+
+								Possuo Certificação Microsoft Office Specialist (MOS)  e trabalho com soluções computacionais para empresas e prestadores de serviços já há mais de 15 anos como Marketing Digital, Suporte Técnico, Otimização em Canais do Youtube, Blogs e Sites.<br>
+
+								Tenho grande prazer em poder ensinar, transmitir conhecimento e claro, participar do crescimento das pessoas.</p>
+							</div>
+						 </div>
+
+	    </div>
+
+	    <div class="container">
+	    	<hr>
+	    	<footer>
+	    		   		
+
+	    		<blockquote class="blockquote text-center">
+  			<h5><strong>Desenvolvido Por: TECTREINAMENTOS CURSOS DE INFORMÁTICA<br> WHATSAPP (11) 9701-33300</strong></h5>
+				</blockquote>
+
+	    	</footer>
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
